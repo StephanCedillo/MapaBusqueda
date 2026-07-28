b6b53d1e3a134589071dcc0b76e91344ad4de9b1
Galito-18 <238720115+galoprieto18-gif@users.noreply.github.com>
2026-07-28 12:37:45 -0500

    Creado Models

---
+++ src/models/MapPoint.java
@@ -0,0 +1,60 @@
+package models;
+
+public class MapPoint {
+
+    private String id;
+    private int x;
+    private int y;
+
+    
+    public MapPoint(String id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MapPoint other = (MapPoint) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+    
+}
+
---
+++ src/models/VisualizationMode.java
@@ -0,0 +1,7 @@
+package models;
+
+public class VisualizationMode {
+    // EXPLORATION, 
+    // FINAL_PATH
+}
+    
